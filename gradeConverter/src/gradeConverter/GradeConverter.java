ee34a9094b6328b3e2cea0c497ae96510b208a5d
ucctn <chetan.singhal@ucertify.com>
2024-02-11 17:09:12 -0800

    Add Grade "A"

--- gradeConverter/src/gradeConverter/GradeConverter.java
+++ gradeConverter/src/gradeConverter/GradeConverter.java
@@ -3,11 +3,32 @@
 //Grade Converter
 
 package gradeConverter;
+import java.util.Scanner;
 
 public class GradeConverter {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		String cont = "Y";
+		String grade = "";
+		int score = 0;
+		
+		while(cont == "Y" || cont == "y")
+		{
+			Scanner gradeOBJ = new Scanner(System.in);
+			System.out.println("Enter numerical grade: ");
+			score = gradeOBJ.nextInt();
+			
+			if (score > 89 && score < 100)
+			{
+				grade = "A";
+				break;
+			}
+			else
+			{
+			}
+			}
+		}
 
 	}
 
