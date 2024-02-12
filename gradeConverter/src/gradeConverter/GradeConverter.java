385077a8fd14fc4e2b2a31b2e0a687dbf955865f
ucctn <chetan.singhal@ucertify.com>
2024-02-11 16:29:39 -0800

    Start Grade Converter

---
+++ gradeConverter/src/gradeConverter/GradeConverter.java
@@ -0,0 +1,14 @@
+//Thora Cummins
+//CIS406
+//Grade Converter
+
+package gradeConverter;
+
+public class GradeConverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
